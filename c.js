2cb630d5ecb58b74ddfb034daee4c89f85f6316f
fernando_R <fer0624@gmail.com>
2018-08-19 17:29:28 -0700

    newone

---
+++ c.js
@@ -0,0 +1,110 @@
+
+
+// 1. Initialize Firebase
+ // Initialize Firebase
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCQMOCI_G8buo_ZsD19AbaFRBbPHZOnPqc",
+    authDomain: "portafoliofr7.firebaseapp.com",
+    databaseURL: "https://portafoliofr7.firebaseio.com",
+    projectId: "portafoliofr7",
+    storageBucket: "portafoliofr7.appspot.com",
+    messagingSenderId: "28348492164"
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+  
+//var clickCounter = 0;
+
+// FUNCTIONS + EVENTS
+// --find analytica to tract times u visited the site------------------------------------------------------------------------------
+
+//$("#click-button").on("click", function() {
+//  clickCounter++;
+//  database.ref().set({
+ //   clickCount: clickCounter
+//  });
+//});
+
+// MAIN PROCESS + INITIAL CODE
+// --------------------------------------------------------------------------------
+
+//database.ref().on("value", function(snapshot) {
+//  console.log(snapshot.val());
+//  $("#click-value").text(snapshot.val().clickCount);
+//  clickCounter = snapshot.val().clickCount;
+//}, function(errorObject) {
+//  console.log("The read failed: " + errorObject.code);
+//});
+
+  
+  // 2. Button for adding Employees
+  $("#add-comment-btn").on("click", function (event) {
+    event.preventDefault();
+  
+    var UName = $("#name-input").val().trim();//tnmae= any value nput it
+  
+    // Time is 3:30 AM
+    var tEmail = $("#email-input").val().trim();//firstime= any value nput it
+  
+    var reason = $("#reason-input").val().trim();//trainD= any value nput it
+  
+  
+    var comment = $("#comment-input").val().trim();//frequency= any value nput it
+  
+   
+  
+    // Creates local "temporary" object for holding employee data
+    var newEmp = {
+      Username: UName,
+      email: tEmail,
+      search: reason,
+      feedback: comment
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newEmp);
+  
+    // Logs everything to console
+    
+  
+    alert("Thank you i will review your inquiry and get back to you");
+  
+    // Clears all of the text-boxes
+    $("#name-input").val("");
+    $("#email-input").val("");
+    $("#reason-input").val("");
+    $("#comment-input").val("");
+  });
+   
+  
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var UName = childSnapshot.val().Username;
+    var tEmail = childSnapshot.val().email;
+    var reason = childSnapshot.val().search;
+    var comment = childSnapshot.val().feedback;
+    
+  
+   
+
+  
+    // Create the new row
+ ////   var newRow = $("<tr>").append(
+ //     $("<td>").text(UName),
+ //     $("<td>").text(tEmail),//for matted for standard time
+ //     $("<td>").text(reason),
+ //     $("<td>").text(comment),
+ //    );
+ // 
+  //  // Append the new row to the table
+ //   $("#user-table > tbody").append(newRow);
+  });
+  
+ 
+  
---
+++ p.js
@@ -0,0 +1,119 @@
+var config = {
+    apiKey: "AIzaSyCQMOCI_G8buo_ZsD19AbaFRBbPHZOnPqc",
+    authDomain: "portafoliofr7.firebaseapp.com",
+    databaseURL: "https://portafoliofr7.firebaseio.com",
+    projectId: "portafoliofr7",
+    storageBucket: "portafoliofr7.appspot.com",
+    messagingSenderId: "28348492164"
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$("#add-comment-btn").on("click", function (event) {
+    event.preventDefault();
+  
+    var feedback = $("#comment-input").val().trim();//tnmae= any value nput it
+  
+       
+  
+    // Creates local "temporary" object for holding employee data
+    var commEnt = {
+      comment: feedback
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(commEnt);
+  
+    // Logs everything to console
+    
+  
+    alert("Thank you for the feedback");
+  
+    // Clears all of the text-boxes
+    
+    $("#comment-input").val("");
+  });
+   
+  
+  // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function (childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+  
+    var feedback = childSnapshot.val().comment;
+    
+  
+   
+
+  
+     //Create the new row
+    var newRow = $("<tr>").append(
+     
+     $("<td>").text(feedback),
+    );
+ 
+  //  // Append the new row to the table
+   $("#user-table > tbody").append(newRow);
+  });
+
+  //time for show
+  // Slideshow Activity
+// Students: follow the instructions below:
+
+// TODO: Put links to our images in this image array.
+var images = ["assets/wel.jpg","assets/moodpass.jpg", "assets/yugi.jpg", "assets/trainschedule.png","assets/giphy.gif"];
+
+// Variable showImage will hold the setInterval when we start the slideshow
+var showImage;
+
+// Count will keep track of the index of the currently displaying picture.
+var count = 0;
+startSlideshow;
+
+// TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
+$("#start").on("click", startSlideshow)
+// TODO: Use jQuery to run "stopSlideshow" when we click the "stop" button.
+$("#stop").on("click", stopSlideshow)
+
+
+// This function will replace display whatever image it's given
+// in the 'src' attribute of the img tag.
+function displayImage() {
+  $("#image-holder").html("<img src=" + images[count] + " width='400px' height='400px'>");
+}
+
+function nextImage() {
+
+  // TODO: Increment the count by 1.
+  count++;
+
+  // TODO: Show the loading gif in the "image-holder" div.
+  $("#image-holder").html("<img src='assets/loading.gif'>");
+
+  // TODO: Use a setTimeout to run displayImage after 1 second.
+  setTimeout(displayImage, 3000);
+
+  // TODO: If the count is the same as the length of the image array, reset the count to 0.
+  if(count == images.length) {
+    count = 0;
+  }
+
+}
+
+
+function startSlideshow() {
+
+  // TODO: Use showImage to hold the setInterval to run nextImage.
+  showImage = setInterval(nextImage, 3000);
+}
+function stopSlideshow() {
+
+  // TODO: Put our clearInterval here:
+  clearInterval(showImage);
+}
+
+// This will run the display image function as soon as the page loads.
+displayImage();
