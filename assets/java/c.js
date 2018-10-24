311136f11c642ed246de01436960adf5c2ca85eb
fernando_R <fer0624@gmail.com>
2018-10-23 23:04:13 -0700

    newupdate

--- assets/java/c.js
+++ assets/java/c.js
@@ -1,4 +1,23 @@
-
+firebase.auth().signInWithRedirect(provider);
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+App.initializeApp()
 
 // 1. Initialize Firebase
  // Initialize Firebase
@@ -111,6 +130,6 @@ var database = firebase.database();
   function(messages) {
     console.log(messages)
   })
-  
+
  
   
